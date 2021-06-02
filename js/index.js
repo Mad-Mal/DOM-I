5905fe3bebfe9bd1ee508c939ff455edd15f8a8d
Mallory <mallorydoescoding@gmail.com>
2021-06-02 09:08:49 -0500

    All selectors created and ready to start manipulating and adding to the DOM

--- js/index.js
+++ js/index.js
@@ -40,3 +40,47 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//navbar and anchors
+
+const navBar = document.getElementsByTagName('nav')
+const navAnchors = navBar.getElementsByTagName('a')
+
+//h1 w/ button and circle img
+
+const h1Dom = document.getElementsByClassName('cta-text')[0]
+const mainButton = document.getElementsByClassName('cta-text')[1]
+const circleImg = document.getElementById('cta-img')
+
+//top content, 2 paragraphs
+
+const topPara1Container = document.getElementsByClassName('top-content')[0]
+const topTitle1 = topPara1Container[0]
+const topPara1 = topPara1Container[1]
+
+const topPara2Container = document.getElementsByClassName('top-content')[1]
+const topTitle2 = topPara2Container[0]
+const topPara2 = topPara2Container[1]
+
+//middle img
+
+const midImg = document.getElementById('middle-img')
+
+//bottom content, 3 paragraphs
+
+const bottomPara1Container = document.getElementsByClassName('bottom-content')[0]
+const bottomTitle1 = bottomPara1Container[0]
+const bottomPara1 = bottomPara1Container[1]
+
+const bottomPara2Container = document.getElementsByClassName('bottom-content')[1]
+const bottomTitle2 = bottomPara2Container[0]
+const bottomPara2 = bottomPara2Container[1]
+
+const bottomPara3Container = document.getElementsByClassName('bottom-content')[2]
+const bottomTitle3 = bottomPara3Container[0]
+const bottomPara3 = bottomPara3Container[1]
+
+//footer
+
+const footerContainer = document.getElementsByTagName('footer')
+const footerPara = footerContainer[0]
